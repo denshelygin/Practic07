50704b86db6e7cea55d5a35c63900d0b628be6f7
Denis <dnnshlgn@icloud.com>
2022-10-08 15:27:22 +0300

    P47

---
+++ Pr47/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+//  заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] arr = new double [m,n];
+for ( int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int  j = 0; j <arr.GetLength(0); j++)
+    { 
+       arr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+    }   
+}
+for ( int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int  j = 0; j <arr.GetLength(0); j++)
+    { 
+       Console.Write(arr[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
